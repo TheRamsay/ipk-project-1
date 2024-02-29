50285a6a38488c1d5f86dfda68b1ca84eceba848
Dominik Huml <xhumld00@vutbr.cz>
2024-02-29 14:19:10 +0100

    WIP: tcp states

--- App/Program.cs
+++ App/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Sockets;
+using System.Text;
 using CommandLine;
+using IPK.Project1.App.Enums;
 
 namespace  IPK.Project1.App;
 
@@ -8,16 +10,47 @@ static class Program
     static void Main(string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(Run);
+            .WithParsed(RunClient);
     }
-    public static void Run(Options opt)
+    public static void RunClient(Options opt)
     {
-        var client = new UdpClient();
+        var client = new TcpClient();
+        var currentState = ProtocolState.Start;
+        
         client.Connect(opt.Host, opt.Port);
-
+        var stream = client.GetStream();
+        stream.ReadTimeout = 2000;
+        
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        
         while (true)
         {
+            // Get user input
+            var line = Console.ReadLine();
             
+            // Send user Command
+            if (line.StartsWith("/"))
+            {
+                var parts = line.Split(" ");
+                var command = parts[0].Substring(1, parts[0].Length - 1);
+                var messageType = Enum.Parse(typeof(MessageType), command);
+            }
+            // Send user message
+            else
+            {
+                var message = line;
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                stream.Write(bytes);
+            }
+            
+            // -------------------------------------------- //
+            
+            // Receive 
+            var receiveBuffer = new byte[2048];
+            var length = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+            var responseData = Encoding.UTF8.GetString(receiveBuffer, 0, length);
+            
+            Console.WriteLine($"[RECEIVED] {responseData}");
         }
 
     }
