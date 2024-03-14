68d0bd3d8a588423cee0afec222d93f0d0737e93
Dominik Huml <xhumld00@vutbr.cz>
2024-03-15 00:04:38 +0100

    WIP: unit tests 😭

---
+++ App.Tests/ChatClientTests.cs
@@ -0,0 +1,22 @@
+﻿using App.input;
+using App.Transport;
+using Xunit;
+using NSubstitute;
+
+namespace App.Tests;
+
+public class ChatClientTests
+{
+    [Fact]
+    public async void AuthTest()
+    {
+        var standardInputReader = Substitute.For<IStandardInputReader>();
+        standardInputReader.ReadLine().Returns("/auth xlogin00 123456 pepik", "/end");
+        
+        var transport = Substitute.For<ITransport>();
+
+        var cancellationTokenSource = new CancellationTokenSource();
+        var client = new ChatClient(transport, standardInputReader, cancellationTokenSource);
+        await client.Start();
+    } 
+}
