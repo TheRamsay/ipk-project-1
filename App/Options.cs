7c9121fece723dae21db2ca9968f4db98a45de3c
Dominik Huml <xhumld00@vutbr.cz>
2024-03-31 22:26:03 +0200

    valid defaults for options

--- App/Options.cs
+++ App/Options.cs
@@ -8,16 +8,16 @@ public class Options
 {
     [Option('t', Required = true, HelpText = "Transport protocol used for connection")]
     public TransportProtocol Protocol { get; set; }
-    
+
     [Option('s', Required = true, HelpText = "Server IP or hostname")]
     public string Host { get; set; }
-    
-    [Option('p', Required = false, Default = 4567, HelpText = "Server port")]
+
+    [Option('p', Required = false, Default = (ushort)4567, HelpText = "Server port")]
     public ushort Port { get; set; }
-    
-    [Option('d', Required = false, Default = 3, HelpText = "UDP confirmation timeout")]
+
+    [Option('d', Required = false, Default = (ushort)250, HelpText = "UDP confirmation timeout")]
     public ushort Timeout { get; set; }
-    
-    [Option('r', Default = 250, Required = false, HelpText = "Maximum number of UDP retransmissions")]
+
+    [Option('r', Default = (byte)3, Required = false, HelpText = "Maximum number of UDP retransmissions")]
     public byte RetryCount { get; set; }
 }
